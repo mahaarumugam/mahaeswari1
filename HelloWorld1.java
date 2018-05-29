2e09533b9c5e7b16bf31ac01cc408289ddf44fd3
mahaarumugam <39696057+mahaarumugam@users.noreply.github.com>
2018-05-29 11:57:34 +0530

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,11 @@
+public class HelloWorld1
+{
+public static void main(String a[])
+{
+System.out.println("welcome to training");
+byte one=25;
+byte two=15;
+int three=one+two;
+System.out.println(three);
+}
+}
